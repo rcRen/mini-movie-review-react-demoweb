9d87addc3e366e7d03972d52451d7c829f99f663
Qichen Zhu <zqc9@hotmail.com>
2022-12-04 00:44:43 -0500

    1204

--- src/components/MovieBox.js
+++ src/components/MovieBox.js
@@ -11,8 +11,8 @@ const MovieBox = ({ title, poster_path, vote_average, release_date, overview }) 
 
     return (
         <div className='card text-center bg-secondary mb-3'>
-            <div className='card-body'>
-                <img className='card-img-top' src={API_IMG + poster_path} alt="img"></img>
+            <div className='card-body' style={{padding:"10px"}}>
+                <img className='card-img' style={{height:'230px', width:'170px'}} src={API_IMG + poster_path} alt="img"></img>
                 <div className='card-body'>
                     <button type="button" className="btn btn-dark" onClick={handleShow}>View Movie</button>
                     <Modal show={show} onHide={handleClose} >
@@ -20,11 +20,12 @@ const MovieBox = ({ title, poster_path, vote_average, release_date, overview }) 
                             <Modal.Title><h3>{title}</h3></Modal.Title>
                         </Modal.Header>
                         <Modal.Body>
-                            <img className='card-img-top' src={API_IMG + poster_path} alt="img"></img>
+                            <img className='rounded mx-auto d-block' style={{height:'400px', width:'350px'}} src={API_IMG + poster_path} alt="img"></img>
                             <br></br>
-                            <h5>IMDb Average: {vote_average}</h5>
-                            <h5>Release Date: {release_date}</h5>
-                            <h6>Overview</h6>
+                            <h5><b>IMDb RATING: {vote_average}</b></h5>                    
+                            <h5><i>Release Date: {release_date}</i></h5>
+                            <hr></hr>
+                            <h6><b>Overview</b></h6>
                             <p>{overview}</p>
                         </Modal.Body>
                         <Modal.Footer>
