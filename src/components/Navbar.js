147bc8c9fcdb61baad6fd7ab989b46331cb4d67e
Qichen Zhu <zqc9@hotmail.com>
2022-12-04 00:21:01 -0500

    1204

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,4 @@
-import React, { useState } from 'react';
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { SidebarData } from './SidebarData';
 import { IconContext } from 'react-icons';
@@ -8,24 +6,15 @@ import { IconContext } from 'react-icons';
 import './Navbar.css';
 
 function Navbar() {
-    const [sidebar, setSidebar] = useState(true);
-    const showSidebar = () => setSidebar(!sidebar);
 
     return (
         <>
-            <IconContext.Provider value={{ color: 'black' }}>
-                <div className="navbar">
-                    <Link to="#" className="menu-bars">
-                        <FaIcons.FaBars onClick={showSidebar} />
-                    </Link>
-                </div>
-            </IconContext.Provider>
             <IconContext.Provider value={{ color: '#fff' }}>
-                <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-                    <ul className="nav-menu-items" onClick={showSidebar}>
+                <nav className='nav-menu active'>
+                    <ul className="nav-menu-items">
                         <li className="navbar-toggle">
-                            <Link to="#" className="menu-bars">
-                                <AiIcons.AiOutlineClose />
+                            <Link to="/" className="menu-bars">
+                                <img src="logo.png" alt="logo" id="logo"></img>
                             </Link>
                         </li>
                         {SidebarData.map((item, index) => {
