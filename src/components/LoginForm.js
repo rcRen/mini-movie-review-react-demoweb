96a53af59db6f00e9bde0979853a764d55a2fa86
Rucheng <rrc@s-3.local>
2022-12-04 20:24:24 -0500

    loginform

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 import MyCard from './UI/MyCard'
 import Form from 'react-bootstrap/Form'
 import MyButton from './UI/MyButton'
 
 function LoginForm() {
+    const [inputEmail, setInputEmail] = useState();
+    const [inputPassword, setInputPassword] = useState();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetch("http://localhost:3001/login", {
+            method: "POST",
+            body: JSON.stringify({
+                email: inputEmail,
+                password: inputPassword,
+            }),
+            headers: {
+                'Content-type': "application/json; charset=UTF-8",
+            }
+        })
+            .then((data) => data.json())
+            .then((json) => {
+                (json.success? alert('Login sucessful') : alert(json.loginErr));
+            });
+    }
+
     return (
         <MyCard>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className='mb-5' controlId="formBasicEmail">
-                    <Form.Control type='email' placeholder='Email' />
+                    <Form.Control type='email' placeholder='Email'
+                        value={inputEmail} onChange={(e) => { setInputEmail(e.target.value) }} />
                 </Form.Group>
                 <Form.Group className='mb-5' controlId="formBasicPassword">
-                    <Form.Control type='password' placeholder='Password' />
+                    <Form.Control type='password' placeholder='Password'
+                        value={inputPassword} onChange={(e) => { setInputPassword(e.target.value) }} />
                 </Form.Group>
                 <MyButton type="submit">
                     Login
