c28b83e3ad542795a50d9837a7fda0e8bbe1a247
Camille-Guo <zibin_guo@hotmail.com>
2022-12-05 11:49:18 -0500

    add theme button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import MovieHome from "./pages/MovieHome";
@@ -13,26 +14,40 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import RegisterLoginPage from "./pages/RegisterLoginPage";
 
-function App(props) {
+function App() {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    document.body.className = theme;
+  }, [theme]);
   return (
     <>
-      {props.children}
-      <Header />
-      <Router>
-        <Navbar />
-        <div class="main" className="container">
-          <Routes>
-            {/* <Route path="/" element={<Home />} /> */}
-            <Route path="/" element={<MovieHome />} />
-            <Route path="/popular" element={<MoviePopular />} />
-            <Route path="/about" element={<About />} />
-            {/* <Route path="/profile" element={isLogin ? <Profile />} : '' /> */}
-            <Route path="/recommend1" element={<Recommend1 />} />
-            <Route path="/recommend2" element={<Recommend2 />} />
-            <Route path="/recommend3" element={<Recommend3 />} />
-          </Routes>
-        </div>
-      </Router>
+      <div className={`App ${theme}`}>
+        <button onClick={toggleTheme}>Toggle Theme</button>
+        <Header />
+        <Router>
+          <Navbar />
+          <div class="main" className="container">
+            <Routes>
+              {/* <Route path="/" element={<Home />} /> */}
+              <Route path="/" element={<MovieHome />} />
+              <Route path="/popular" element={<MoviePopular />} />
+              <Route path="/about" element={<About />} />
+              {/* <Route path="/profile" element={isLogin ? <Profile />} : '' /> */}
+              <Route path="/recommend1" element={<Recommend1 />} />
+              <Route path="/recommend2" element={<Recommend2 />} />
+              <Route path="/recommend3" element={<Recommend3 />} />
+            </Routes>
+          </div>
+        </Router>
+      </div>
     </>
   );
 }
