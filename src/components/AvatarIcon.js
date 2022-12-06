fb251a4cf863f6b3b3caef0bacb4c46fc64bc0ec
Programxiaoming <xiaomingsu@outlook.com>
2022-12-06 17:44:20 -0500

    avatar icon

--- src/components/AvatarIcon.js
+++ src/components/AvatarIcon.js
@@ -14,25 +14,12 @@ function AvatarIcon() {
   };
   return (
     <div style={{ flexDirection: "row", direction: "rtl" }}>
-      <Button style={{backgroundImage:"url('./images/person.png')",backgroundSize:"cover", width:"40px", height:"40px"}}onClick={handleSubmit}></Button>
+      <Button style={{backgroundColor:"white",backgroundSize:"cover"}}onClick={handleSubmit}>
+        <img src="./images/person.png" width="30" height="30" alt=""/>
+      </Button>
     </div>
   );
 
-/**
-<svg aria-hidden="true" focusable="false" data-prefix="fab"
-data-icon="user" class="svg-inline--fa fa-user" role="img" 
-xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
-</svg>
-
-  return (<div>
-    <span className="e-avatar e-avatar-xlarge e-avatar-circle green">RC</span>
-    <span className="e-avatar e-avatar-xlarge e-avatar-circle violet">QC</span>
-    <span className="e-avatar e-avatar-xlarge e-avatar-circle rose">ZB</span>
-    <span className="e-avatar e-avatar-xlarge e-avatar-circle blue">XM</span>
-
-</div>);
-
- */
 }
 
 export default AvatarIcon;
