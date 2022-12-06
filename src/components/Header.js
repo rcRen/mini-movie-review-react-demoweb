d52018b36984033b97a3937123e3daa0c570ed2e
Rucheng <rrc@s-3.local>
2022-12-06 17:01:29 -0500

    track main

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,24 +6,58 @@ import './Header.css';
 import RegisterButton from './RegisterButton';
 import LoginButton from './LoginButtton';
 import LogoutButton from './LogoutButton';
+import React from 'react';
+import { Navbar, Container, Nav, Form, FormControl, Button } from 'react-bootstrap';
 
-const Header = () => {
-	const userInfo = User.getUser();
 
-	return (
-		<div class="header">
-			<div style={{ float: 'right' }}>
-				{userInfo ? (
-					<LogoutButton />
-				) : (
-					<>
-						<LoginButton />
-						<RegisterButton />
-					</>
-				)}
-			</div>
-		</div>
-	);
-};
+function Header() {
+    return (
+        <Navbar expand='lg' style={{ marginTop: '2%', marginRight: '2%', marginBottom: '4%' }}>
+            <Container fluid>
+                <Navbar.Brand href='#'></Navbar.Brand>
+                <Navbar.Toggle aria-controls='navbarScroll'></Navbar.Toggle>
+                <Navbar.Collapse id='navbarScroll'>
+                    <Nav className='me-auto my-2 my-lg-3'
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll></Nav>
 
-export default Header;
+                    <Form className='d-flex'>
+                        <FormControl
+                            type='search'
+                            className='me-2'
+                            aria-label='search'
+                            style={{ marginLeft: '5%' }}
+                            name=''>
+                        </FormControl>
+                        <Button variant='success' type='submit'>Search</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+// export default Header;
+
+
+
+// const Header = () => {
+// 	const userInfo = User.getUser();
+
+// 	return (
+// 		<div class="header">
+// 			<div style={{ float: 'right' }}>
+// 				{userInfo ? (
+// 					<LogoutButton />
+// 				) : (
+// 					<>
+// 						<LoginButton />
+// 						<RegisterButton />
+// 					</>
+// 				)}
+// 			</div>
+// 		</div>
+// 	);
+// };
+
+// export default Header;
--- src/components/MovieBox.js
+++ src/components/MovieBox.js
@@ -1,41 +1,30 @@
 import { Modal, Button } from 'react-bootstrap';
 import React, { useState } from 'react';
-import DisplayMovieInfo from './DisplayMovieInfo';
 
 const API_IMG = "https://image.tmdb.org/t/p/w500";
 const MovieBox = ({ title, poster_path, vote_average, release_date, overview }) => {
+
     const [show, setShow] = useState(false);
 
     const handleShow = () => setShow(true);
     const handleClose = () => setShow(false);
 
-
     return (
         <div className='card text-center bg-secondary mb-3'>
-            <div className='card-body' 
-                 style={{padding:"0 10px"}}>
-                <img className='card-img' 
-                     style={{height:'70%', width:'100%'}} 
-                     src={API_IMG + poster_path} alt="img">
-                </img>
+            <div className='card-body'>
+                <img className='card-img-top' src={API_IMG + poster_path} alt="img"></img>
                 <div className='card-body'>
-                    <button type="button" 
-                            style={{height:'15%', width:'70%', position: 'absolute', bottom:'5%', left:'15%'}} 
-                            className="btn btn-dark" onClick={handleShow}>View
-                    </button>
+                    <button type="button" className="btn btn-dark" onClick={handleShow}>View Movie</button>
                     <Modal show={show} onHide={handleClose} >
                         <Modal.Header closeButton>
                             <Modal.Title><h3>{title}</h3></Modal.Title>
                         </Modal.Header>
                         <Modal.Body>
-                            <img className='rounded mx-auto d-block' 
-                                 style={{height:'50%', width:'60%'}} 
-                                 src={API_IMG + poster_path} alt="img"></img>
+                            <img className='card-img-top' style={{width:'14rem'}} src={API_IMG + poster_path} alt="img"></img>
                             <br></br>
-                            <h5><b>IMDb RATING: {vote_average}</b></h5>                    
-                            <h5><i>Release Date: {release_date}</i></h5>
-                            <hr></hr>
-                            <h6><b>Overview</b></h6>
+                            <h5>IMDb Average: {vote_average}</h5>
+                            <h5>Release Date: {release_date}</h5>
+                            <h6>Overview</h6>
                             <p>{overview}</p>
                         </Modal.Body>
                         <Modal.Footer>
