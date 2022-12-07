73442c1df0655230d80ad4e97ea17db09ad19a9b
Camille-Guo <zibin_guo@hotmail.com>
2022-12-07 11:29:35 -0500

    first version merge main

--- src/App.js
+++ src/App.js
@@ -1,68 +1,76 @@
-import "./App.css";
-import React, { useState, useEffect } from "react";
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import MovieHome from "./pages/MovieHome";
-import MoviePopular from "./pages/MoviePopular";
-import About from "./pages/About";
-import MyReview from "./pages/MyReview";
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header';
+// import Navbar from "./components/Navbar";
+import MovieHome from './pages/MovieHome';
+import MoviePopular from './pages/MoviePopular';
+import About from './pages/About';
+import MyReview from './pages/MyReview';
 // import Profile from './pages/Profile';
-import AddReview from "./pages/AddReview";
-import EditReviewForm from "./components/EditReviewForm";
+import AddReview from './pages/AddReview';
+import EditReviewForm from './components/EditReviewForm';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Sidebar from './components/Sidebar';
 import Movie1 from './pages/Movie1';
 import Movie2 from './pages/Movie2';
 import Movie3 from './pages/Movie3';
-import RegisterLoginPage from "./pages/RegisterLoginPage";
-import { StarRating } from "./components/StarRating";
-import AddReviewButton from "./components/AddReviewButton";
-import MyReviewButton from "./components/MyReviewButton";
-import SearchHome from "./pages/SearchHome";
+// import RegisterLoginPage from "./pages/RegisterLoginPage";
+import { StarRating } from './components/StarRating';
+import AddReviewButton from './components/AddReviewButton';
+import MyReviewButton from './components/MyReviewButton';
+import SearchHome from './pages/SearchHome';
 
 function App() {
-  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
-  const toggleTheme = () => {
-    if (theme === "light") {
-      setTheme("dark");
-    } else {
-      setTheme("light");
-    }
-  };
-  useEffect(() => {
-    localStorage.setItem("theme", theme);
-    document.body.className = theme;
-  }, [theme]);
-  return (
-    <>
-      <div className={`App ${theme}`}>
-        <Router>
-          <Header />
-          <button onClick={toggleTheme}>Toggle Theme</button>
-          <AddReviewButton />
-          <MyReviewButton />
-          {/* <Navbar /> */}
-          <div class="main" className="container">
-            <Routes>
-              {/* <Route path="/" element={<Home />} /> */}
-              <Route path="/" element={<MovieHome />} />
-              <Route path="/popular" element={<MoviePopular />} />
-              <Route path="/about" element={<About />} />
-              {/* <Route path="/profile" element={isLogin ? <Profile />} : '' /> */}
-              <Route path="/movie1" element={<Movie1 />} />
-            <Route path="/movie2" element={<Movie2 />} />
-            <Route path="/movie3" element={<Movie3 />} />
-              <Route path="/addreview" element={<AddReview />} />
-              <Route path="/myreview" element={<MyReview />} />
-              <Route path="/searchhome" element={<SearchHome />} />
-              <Route path="/edit/:id" element={<EditReviewForm />} />
-            </Routes>
-          </div>
-        </Router>
-      </div>
-    </>
-  );
+	const [theme, setTheme] = useState(
+		localStorage.getItem('theme') || 'light'
+	);
+	const toggleTheme = () => {
+		if (theme === 'light') {
+			setTheme('dark');
+		} else {
+			setTheme('light');
+		}
+	};
+	useEffect(() => {
+		localStorage.setItem('theme', theme);
+		document.body.className = theme;
+	}, [theme]);
+	return (
+		<>
+			<div className={`App ${theme}`}>
+				<Router>
+					<Header />
+					<button onClick={toggleTheme}>Toggle Theme</button>
+					<AddReviewButton />
+					<MyReviewButton />
+					{/* <Navbar /> */}
+					<div class="main" className="container">
+						<Routes>
+							{/* <Route path="/" element={<Home />} /> */}
+							<Route path="/" element={<MovieHome />} />
+							<Route path="/popular" element={<MoviePopular />} />
+							<Route path="/about" element={<About />} />
+							{/* <Route path="/profile" element={isLogin ? <Profile />} : '' /> */}
+							<Route path="/movie1" element={<Movie1 />} />
+							<Route path="/movie2" element={<Movie2 />} />
+							<Route path="/movie3" element={<Movie3 />} />
+							<Route path="/addreview" element={<AddReview />} />
+							<Route path="/myreview" element={<MyReview />} />
+							<Route
+								path="/searchhome"
+								element={<SearchHome />}
+							/>
+							<Route
+								path="/edit/:id"
+								element={<EditReviewForm />}
+							/>
+						</Routes>
+					</div>
+				</Router>
+			</div>
+		</>
+	);
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,77 +1,82 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import "../App.css";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../App.css';
 import {
-  Container,
-  Form,
-  FormControl,
-  Nav,
-  Navbar,
-  Button,
-} from "react-bootstrap";
-import "./Header.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+	Container,
+	Form,
+	FormControl,
+	Nav,
+	Navbar,
+	Button,
+} from 'react-bootstrap';
+import { User } from '../helpers/LocalStorage';
+import RegisterButton from './RegisterButton';
+import LoginButton from './LoginButtton';
+import LogoutButton from './LogoutButton';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => {
-  const navigate = useNavigate();
-  const [query, setQuery] = useState("");
+	const navigate = useNavigate();
+	const [query, setQuery] = useState('');
 
-  const searchMovie = async (e) => {
-    e.preventDefault();
-    console.log("Searching");
-    // alert(query);
-    // alert(localStorage.getItem("query"));
-    try {
-      // navigation.navigate(name, params)
+	const searchMovie = async (e) => {
+		e.preventDefault();
+		console.log('Searching');
+		// alert(query);
+		// alert(localStorage.getItem("query"));
+		try {
+			// navigation.navigate(name, params)
 
-      navigate("/searchhome", { state: { query } });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  const changeHandler = (e) => {
-    setQuery(e.target.value);
-    // alert(query);
-    // localStorage.setItem("query", query);
-  };
+			navigate('/searchhome', { state: { query } });
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	const changeHandler = (e) => {
+		setQuery(e.target.value);
+		// alert(query);
+		// localStorage.setItem("query", query);
+	};
 
-  return (
-    <>
-      <Navbar
-        // bg="light"
-        expand="lg"
-        // variant="light"
-        // style={{ position: "absolute", right: "20%", color: "red" }}
-      >
-        <Container fluid>
-          <Navbar.Brand href="">Welcome</Navbar.Brand>
-          <Navbar.Brand href="">Trending</Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll"> </Navbar.Toggle>
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-3"
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            ></Nav>
-            <Form className="d-flex" onSubmit={searchMovie}>
-              <FormControl
-                type="search"
-                placeholder="Movie Search"
-                className="me-2"
-                aria-label="search"
-                name="query"
-                value={query}
-                onChange={changeHandler}
-              ></FormControl>
-              <Button variant="secondary" type="submit">
-                Search
-              </Button>
-            </Form>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </>
-  );
+	return (
+		<>
+			<Navbar
+				// bg="light"
+				expand="lg"
+				// variant="light"
+				// style={{ position: "absolute", right: "20%", color: "red" }}
+			>
+				<Container fluid>
+					<Navbar.Brand href="">Welcome</Navbar.Brand>
+					<Navbar.Brand href="">Trending</Navbar.Brand>
+					<Navbar.Toggle aria-controls="navbarScroll">
+						{' '}
+					</Navbar.Toggle>
+					<Navbar.Collapse id="navbarScroll">
+						<Nav
+							className="me-auto my-2 my-lg-3"
+							style={{ maxHeight: '100px' }}
+							navbarScroll
+						></Nav>
+						<Form className="d-flex" onSubmit={searchMovie}>
+							<FormControl
+								type="search"
+								placeholder="Movie Search"
+								className="me-2"
+								aria-label="search"
+								name="query"
+								value={query}
+								onChange={changeHandler}
+							></FormControl>
+							<Button variant="secondary" type="submit">
+								Search
+							</Button>
+						</Form>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</>
+	);
 };
 
 export default Header;
