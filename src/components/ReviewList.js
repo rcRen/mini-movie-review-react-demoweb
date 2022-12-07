8fce69633c13f7be289f5641d6a0d96281acac9f
Camille-Guo <zibin_guo@hotmail.com>
2022-12-06 20:44:44 -0500

    delete review

--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import "../App.css";
+import Button from "react-bootstrap/Button";
+import moment from "moment";
+import { Link } from "react-router-dom";
+
 function ReviewList() {
   const [data, setData] = useState([]);
   const [userId, setUserId] = useState(
@@ -20,37 +25,69 @@ function ReviewList() {
       .then((actualData) => {
         console.log(actualData);
         setData(actualData);
-        console.log(data);
       })
       .catch((err) => {
         console.log(err.message);
       });
   };
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetchData();
-  }, []);
+  });
+
+  const handleSubmit = (recordId) => {
+    // e.preventDefault();
+    alert(recordId);
+    console.log({ recordId });
+    fetch("http://localhost:3001/commandreviews/" + recordId, {
+      method: "DELETE",
+    })
+      .then((data) => data.json())
+      .then((json) => alert(JSON.stringify(json)));
+
+    try {
+      navigate("/myreview", { replace: true });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="App">
-      <tbody>
-        <tr>
-          <th>MovieId</th>
-          <th>userId</th>
-          <th>username</th>
-          <th>Rate</th>
-          <th>Content</th>
-        </tr>
-        {data.map((item, index) => (
-          <tr key={index}>
-            <td>{item.movieId}</td>
-            <td>{item.userId}</td>
-            <td>{item.username}</td>
-            <td>{item.rate}</td>
-            <td>{item.content}</td>
+      <table>
+        <tbody>
+          <tr>
+            <th>Movie Id</th>
+            <th>User Id</th>
+            <th>Username</th>
+            <th>Rate</th>
+            <th>Content</th>
+            <th>Update Date</th>
+            <th>Action</th>
           </tr>
-        ))}
-      </tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td>{item.movieId}</td>
+              <td>{item.userId}</td>
+              <td>{item.username}</td>
+              <td>{item.rate}</td>
+              <td>{item.content}</td>
+              <td>{moment(item.updateDate).format("DD-MM-YYYY hh:mm:ss a")}</td>
+              <td>
+                <Link to={"/edit/" + item._id}>Edit</Link>
+                <Button
+                  variant="primary"
+                  value={item._id}
+                  onClick={(e) => handleSubmit(e.target.value)}
+                >
+                  DELETE
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
