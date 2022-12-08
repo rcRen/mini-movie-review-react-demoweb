8240c407c7ba967e3d3ebc14759396a71948c04b
Qichen Zhu <zqc9@hotmail.com>
2022-12-08 01:49:12 -0500

    1208

--- src/components/ThemeButton.js
+++ src/components/ThemeButton.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Button from 'react-bootstrap/Button';
+import { Button } from "react-bootstrap";
 import * as GiIcons from 'react-icons/gi';
 
 function ThemeButton() {
@@ -17,16 +17,9 @@ function ThemeButton() {
   }, [theme]);
   return (
     <>
-      <div className={`App ${theme}`}>
-        <GiIcons.GiCardExchange
-          style={{
-            color: 'black',
-            width: '50px',
-            height: '28px',
-            backgroundColor: 'beige',
-          }}
-          onClick={toggleTheme} />
-      </div>
+      <Button variant="outline-warning" onClick={toggleTheme}>
+        <GiIcons.GiCardExchange style={{ width: '20px', height: '20px' }} />
+      </Button>
     </>
   );
 }
